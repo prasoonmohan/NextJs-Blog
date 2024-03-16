9bda96551a9dee6dc3ef49120ebfea6907227eb8
Prasoon M <prasoon@aufaitux.com>
2024-03-17 00:35:02 +0530

    feat: Adding react hook forms and i=input fields

--- src/Components/FormPost.tsx
+++ src/Components/FormPost.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const FormPost = () => {
     return (
-        <div>
-
-        </div>
+        <form action="" className='flex flex-col items-center justify-center gap-5 mt-10'>
+            <input type="text" placeholder="post title..." className="input input-bordered w-full max-w-lg" />
+            <textarea className="textarea textarea-bordered w-full max-w-lg" placeholder="post content..."></textarea>
+            <select className="select select-bordered w-full max-w-lg">
+                <option disabled selected>Who shot first?</option>
+                <option>Han Solo</option>
+                <option>Greedo</option>
+            </select>
+        </form>
     )
 }
 
