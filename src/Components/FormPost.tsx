6eb35525b32bf9db0ccff051e1ffc1c2b19d3e11
Prasoon M <prasoon@aufaitux.com>
2024-03-17 11:36:34 +0530

    feat: Added form submit

--- src/Components/FormPost.tsx
+++ src/Components/FormPost.tsx
@@ -1,15 +1,27 @@
+"use client"
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const FormPost = () => {
+    const { register, handleSubmit } = useForm();
+    const submit = (data: any) => {
+        console.log(data)
+    }
     return (
-        <form action="" className='flex flex-col items-center justify-center gap-5 mt-10'>
-            <input type="text" placeholder="post title..." className="input input-bordered w-full max-w-lg" />
-            <textarea className="textarea textarea-bordered w-full max-w-lg" placeholder="post content..."></textarea>
-            <select className="select select-bordered w-full max-w-lg">
-                <option disabled selected>Who shot first?</option>
+        <form onSubmit={handleSubmit(submit)} action="" className='flex flex-col items-center justify-center gap-5 mt-10'>
+            <input type="text" placeholder="post title..."
+                {...register("title")}
+                className="input input-bordered w-full max-w-lg" />
+            <textarea className="textarea textarea-bordered w-full max-w-lg"
+                {...register("content")}
+                placeholder="post content..."></textarea>
+            <select {...register("tag")} className="select select-bordered w-full max-w-lg">
+                <option disabled selected>select tags</option>
                 <option>Han Solo</option>
                 <option>Greedo</option>
             </select>
+
+            <button className='btn btn-primary w-full max-w-lg'>Create</button>
         </form>
     )
 }
